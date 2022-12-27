164f7262e4116e57c199b56f8a0f169093989269
Howl404 <114860794+Howl404@users.noreply.github.com>
2022-12-27 18:43:18 +0200

    Prompt for player, 5 rounds with game function

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
-const getComputerChoice = function () {
+function getComputerChoice() {
   // Returns random from Rock, Paper, Scissors
-  const computerChoice = ["rock", "paper", "scissors"];
-  return computerChoice[Math.floor(Math.random() * computerChoice.length)];
-};
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
+  a = ["rock", "paper", "scissors"];
+  b = a[Math.floor(Math.random() * 3)];
+  return b;
+}
 
-const playRound = function (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   console.log(computerSelection);
   b = playerSelection.toLowerCase();
   console.log(b);
+  if (b === computerSelection) {
+    return "Draw!";
+  }
   if (b === "rock") {
-    if (b === computerSelection) {
-      return "Draw!";
-    } else if (computerSelection === "paper") {
+    if (computerSelection === "paper") {
       return "Computer wins!";
     } else if (computerSelection === "scissors") {
       return "Player wins!";
     }
   } else if (b === "paper") {
-    if (b === computerSelection) {
-      return "Draw!";
-    } else if (computerSelection === "rock") {
+    if (computerSelection === "rock") {
       return "Player wins!";
     } else if (computerSelection === "scissors") {
       return "Computer wins!";
     }
   } else if (b === "scissors") {
-    if (b === computerSelection) {
-      return "Draw!";
-    } else if (computerSelection === "paper") {
+    if (computerSelection === "paper") {
       return "Player wins!";
     } else if (computerSelection === "rock") {
       return "Computer wins!";
     }
   }
-};
+}
 
-console.log(playRound(playerSelection, computerSelection));
+let computerSelection = getComputerChoice();
+const playerSelection = prompt("Player selection");
+
+function game() {
+  let player = 0;
+  let computer = 0;
+  for (let i = 0; i < 5; i++) {
+    let result = playRound(playerSelection, getComputerChoice());
+    console.log(result);
+    if (result === "Computer wins!") {
+      computer++;
+    } else if (result === "Player wins!") {
+      player++;
+    }
+  }
+  return `Game is finished, score is - Player ${player}, ${computer} Computer`;
+}
+
+console.log(playRound(playerSelection, getComputerChoice()));
