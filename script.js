6828b6595f05081c429fda40be0df6e3ca06e5a2
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-01-09 13:41:14 +0200

    UI initial

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let gameState = true;
+
 function getComputerChoice() {
   // Returns random from Rock, Paper, Scissors
   a = ["rock", "paper", "scissors"];
@@ -6,35 +10,39 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  console.log(computerSelection);
   b = playerSelection.toLowerCase();
-  console.log(b);
   if (b === computerSelection) {
     return "Draw!";
   }
   if (b === "rock") {
     if (computerSelection === "paper") {
+      computerScore++;
       return "Computer wins!";
     } else if (computerSelection === "scissors") {
+      playerScore++;
       return "Player wins!";
     }
   } else if (b === "paper") {
     if (computerSelection === "rock") {
+      playerScore++;
       return "Player wins!";
     } else if (computerSelection === "scissors") {
+      computerScore++;
       return "Computer wins!";
     }
   } else if (b === "scissors") {
     if (computerSelection === "paper") {
+      playerScore++;
       return "Player wins!";
     } else if (computerSelection === "rock") {
+      computerScore++;
       return "Computer wins!";
     }
   }
 }
 
 let computerSelection = getComputerChoice();
-const playerSelection = prompt("Player selection");
+// const playerSelection = prompt("Player selection");
 
 function game() {
   let player = 0;
@@ -51,4 +59,67 @@ function game() {
   return `Game is finished, score is - Player ${player}, ${computer} Computer`;
 }
 
-console.log(playRound(playerSelection, getComputerChoice()));
+// console.log(playRound(playerSelection, getComputerChoice()));
+
+if (gameState === true) {
+  document.querySelector(".rock").addEventListener("click", function () {
+    if (computerScore >= 5) {
+      gameState = false;
+      document.querySelector(
+        ".result"
+      ).innerHTML = `Computer wins the round! ${computerScore} : ${playerScore}`;
+    } else if (playerScore >= 5) {
+      gameState = false;
+      document.querySelector(
+        ".result"
+      ).innerHTML = `Player wins the round! ${playerScore} : ${computerScore}`;
+    } else {
+      document.querySelector(".result").innerHTML = playRound(
+        "rock",
+        getComputerChoice()
+      );
+      document.querySelector(".player").innerHTML = playerScore;
+      document.querySelector(".computer").innerHTML = computerScore;
+    }
+  });
+  document.querySelector(".paper").addEventListener("click", function () {
+    if (computerScore >= 5) {
+      gameState = false;
+      document.querySelector(
+        ".result"
+      ).innerHTML = `Computer wins the round! ${computerScore} : ${playerScore}`;
+    } else if (playerScore >= 5) {
+      gameState = false;
+      document.querySelector(
+        ".result"
+      ).innerHTML = `Player wins the round! ${playerScore} : ${computerScore}`;
+    } else {
+      document.querySelector(".result").innerHTML = playRound(
+        "paper",
+        getComputerChoice()
+      );
+      document.querySelector(".player").innerHTML = playerScore;
+      document.querySelector(".computer").innerHTML = computerScore;
+    }
+  });
+  document.querySelector(".scissors").addEventListener("click", function () {
+    if (computerScore >= 5) {
+      gameState = false;
+      document.querySelector(
+        ".result"
+      ).innerHTML = `Computer wins the round! ${computerScore} : ${playerScore}`;
+    } else if (playerScore >= 5) {
+      gameState = false;
+      document.querySelector(
+        ".result"
+      ).innerHTML = `Player wins the round! ${playerScore} : ${computerScore}`;
+    } else {
+      document.querySelector(".result").innerHTML = playRound(
+        "scissors",
+        getComputerChoice()
+      );
+      document.querySelector(".player").innerHTML = playerScore;
+      document.querySelector(".computer").innerHTML = computerScore;
+    }
+  });
+}
